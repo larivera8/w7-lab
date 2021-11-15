cc39213732911f168eccb78896ceda194d30f82b
larivera8 <92804930+larivera8@users.noreply.github.com>
2021-11-15 00:07:29 -0600

    Add files via upload

---
+++ w7 css 225 pr1.py
@@ -0,0 +1,13 @@
+#luis rivera
+#11/14/21
+
+#problem 1 is finding the area of a circle
+
+import math  
+math.pi 
+def areaofcricle(r):
+    area = math.pi * (r ** 2)
+    return area 
+
+Radius = int(input("enter a radius:"))
+print("The area of a circle: ", areaOfCircle(4))
